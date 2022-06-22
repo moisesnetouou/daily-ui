6c60bbd51887528f1aac3231e9f5a92b0297df72
Moisés Neto <moisesnetored@gmail.com>
2022-06-21 21:52:29 -0400

    style: SignUp styling, understanding TailwindCSS, Glasmorphism, background image and gradient concepts.

--- src/pages/Challenges/001-sign-up/index.tsx
+++ src/pages/Challenges/001-sign-up/index.tsx
@@ -1,9 +1,14 @@
+import medusaImg from '../../../assets/images/medusa.jpg';
+
 export function SignUp() {
   return (
-    <main>
-      <div>
-        <div>
-          <div className="apresentation-header">
+    <main className="flex h-screen">
+      <div
+        style={{ backgroundImage: `url(${medusaImg})` }}
+        className="flex w-1/2 bg-cover bg-center"
+      >
+        <div className="flex flex-col justify-between h-4/5 m-auto rounded-2xl bg-gradient-to-r from-indigo-500 bg-opacity-20 p-8">
+          <div className="text-white">
             <h1>Delta Deep Sea</h1>
 
             <h2>We invite you to know everything about the Abyssal Beings.</h2>
@@ -13,14 +18,14 @@ export function SignUp() {
             </span>
           </div>
 
-          <div className="apresentation-signin">
+          <div className="flex flex-col">
             <span>Already have an account?</span>
             <a href="/#">Sign In</a>
           </div>
         </div>
       </div>
 
-      <div>1</div>
+      <div className="flex w-1/2">1</div>
     </main>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 export function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1 className="text-indigo-500">Home</h1>
       <a href="/challenge/001">Sign Up</a>
     </main>
   );
